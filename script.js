91f48ad970405faebc92609fa3ec50598ee7891a
Aless <sankash346@gmail.com>
2025-05-20 18:14:35 +0200

    test commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const numberOfFilms = prompt("What's your name?", "");
+
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+}
+
+const nameMovie = prompt("What's your favorite film?"),
+      vuoto = prompt("How was good is it?");
+
+personalMovieDB.movies[nameMovie] = vuoto;
+
+console.log(personalMovieDB["movies"]);
+
+
+
+
