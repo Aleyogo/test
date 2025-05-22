660e20ec43b4bed4d5a689189e4ddc1b13075ac1
Aless <sankash346@gmail.com>
2025-05-22 18:25:42 +0200

    test commit

--- Js/cycles.js
+++ Js/cycles.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let num = 50;
+// let num = 50;
 
 // while(num < 55){
 //     console.log(num);
@@ -13,7 +13,64 @@ let num = 50;
 // }
 // while(num < 55);
 
-for(let i = 1; i < 8; i++){
-    console.log(num);
-    num++;
+// for(let i = 1; i < 8; i++){
+//     if(i === 6){
+//         // break;
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+
+
+
+
+// for(let i = 0; i < 3; i++){
+//     console.log(i);
+//     for(let j = 0; j < 3; j++){
+//         console.log(j);
+//     }
+// }
+
+
+// let result = '';
+// const length = 7;
+
+// for(let i = 1; i < length; i++){
+//     for(let j = 0; j < i; j++){
+//         result += '*';
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+
+// first: for(let i = 0; i < 3; i++){
+//     console.log(`First level: ${i}`);
+//     for(let j = 0; j < 3; j++){
+//         console.log(`Seond level: ${j}`);
+//         for(let k = 0; k < 3; k++){
+//             if(k === 2) continue first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+
+for(let i = data.length - 1; i >= 0; i--){
+    result[i] = data[i];
+    console.log(result[i]);
 }
+
+// console.log(result);
+
+
+
+
