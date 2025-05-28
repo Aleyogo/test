1b1f0c2e4f48c605a532f3c2e99a296b56822ad0
Aless <sankash346@gmail.com>
2025-05-28 20:31:24 +0200

    test commit

--- Js/function.js
+++ Js/function.js
@@ -1,2 +1,73 @@
 "use strict";
 
+// function showFirstMessage(text){
+//     console.log(text);
+//     let num = 20;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello world!");
+
+// // function calc(a, b){
+// //     return(a + b);
+// // }
+
+// console.log(calc(4, 3));
+
+// function ret(){
+//     let num = 50;
+
+//     //
+
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function(){
+//     console.log("Hello!");
+// };
+
+// logger();
+
+// const calcOne = (a, b) => a + b;
+// const calcTwo = (a, b) => { return a + b };
+
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert(amount, curr){
+//     console.log(curr * amount);
+// }
+
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return promotion(curr * amount);
+// }
+
+// function promotion(result){
+//     console.log(result * discount);
+// }
+ 
+// convert(500, usdCurr);
+
+
+// function test(){
+//     for(let i = 0; i < 5; i++){
+//         console.log(i);
+//         if(i === 3) return
+//     }
+//     console.log("Done");
+// } 
+
+// test();
+
+function doNothing(){}
+console.log(doNothing() === undefined);
